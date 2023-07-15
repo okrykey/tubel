f39105ae16a6396c9c50515d956db5ef32098599
RyoOkumura <o9mura2000z@gmail.com>
2023-07-15 18:00:34 +0900

    Controlled behaviors of custom application and document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,72 @@
-import { type Session } from "next-auth";
+import { useState } from "react";
+import NextApp, { AppProps, AppContext } from "next/app";
+import { getCookie, setCookie } from "cookies-next";
+import Head from "next/head";
+import {
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider,
+} from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
+import { Toaster } from "react-hot-toast";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { Session } from "next-auth";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+function MyApp(
+  props: AppProps & { colorScheme: ColorScheme; session: Session | null }
+) {
+  const { Component, pageProps, session } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme
   );
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
+    setCookie("mantine-color-scheme", nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
+  return (
+    <>
+      <Head>
+        <title>My App</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <SessionProvider session={session}>
+        <ColorSchemeProvider
+          colorScheme={colorScheme}
+          toggleColorScheme={toggleColorScheme}
+        >
+          <MantineProvider
+            theme={{ colorScheme }}
+            withGlobalStyles
+            withNormalizeCSS
+          >
+            <Toaster />
+            <Component {...pageProps} />
+            <Notifications />
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </SessionProvider>
+    </>
+  );
+}
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await NextApp.getInitialProps(appContext);
+  return {
+    ...appProps,
+    colorScheme: getCookie("mantine-color-scheme", appContext.ctx) || "dark",
+  };
 };
 
 export default api.withTRPC(MyApp);
