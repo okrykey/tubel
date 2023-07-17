369201e53aeb9b2918e0477a6403769d661746ab
RyoOkumura <o9mura2000z@gmail.com>
2023-07-17 16:11:42 +0900

    Added external packages

---
+++ src/pages/state/Atoms.ts
@@ -0,0 +1,9 @@
+import { atom } from "jotai";
+
+export const modalOpenAtom = atom(false);
+
+export const CommentOpenAtom = atom(false);
+
+export const UrlSetAtom = atom("");
+
+export const IdSetAtom = atom("");
