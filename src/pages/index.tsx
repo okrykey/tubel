2da7c820a83ff8d73c584ae47cea933f73491407
RyoOkumura <o9mura2000z@gmail.com>
2023-08-01 23:35:46 +0900

    modified to display toast upon login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,35 @@
-import { Container, SimpleGrid } from "@mantine/core";
+import { Container } from "@mantine/core";
 import { NextPage } from "next";
-
-import { InputWithButton } from "~/components/Mantine UI/InputWithButton";
-import { Hero } from "~/components/Mantine UI/Hero";
+import { Hero } from "~/components/Hero";
 import PostFormModal from "~/components/PostFormModal";
 import { HeaderTabs } from "~/components/Header";
 import { PostsList } from "~/components/PostsList";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
+import MainLayout from "~/layouts/Mainlayout";
 
 const Home: NextPage = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated" && router.query.login === "success") {
+      toast.success("ログインしました");
+      router.replace(router.pathname);
+    }
+  }, [status, router.query, router.pathname]);
+
   return (
     <>
-      <div className="flex h-screen w-full flex-col">
-        <HeaderTabs />
+      <MainLayout>
         <Container size="lg" py="xl">
-          <PostFormModal></PostFormModal>
-          <InputWithButton className="w-full" />
+          <PostFormModal />
           <Hero />
           <PostsList />
         </Container>
-      </div>
+      </MainLayout>
     </>
   );
 };
