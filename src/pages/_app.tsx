e504927b7e61824c3722d2ce0fa4355cf133a2ab
RyoOkumura <o9mura2000z@gmail.com>
2023-08-04 23:58:58 +0900

    changed Mantine's primary color to indigo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -48,14 +48,30 @@ function MyApp(
           toggleColorScheme={toggleColorScheme}
         >
           <MantineProvider
-            theme={{ colorScheme }}
+            theme={{
+              colorScheme,
+              colors: {
+                indigo: [
+                  "#EDF2FF",
+                  "#DBE4FF",
+                  "#BAC8FF",
+                  "#91A7FF",
+                  "#748FFC",
+                  "#5C7CFA",
+                  "#4C6EF5",
+                  "#4263EB",
+                  "#3B5BDB",
+                  "#364FC7",
+                ],
+              },
+              primaryColor: "indigo",
+            }}
             withGlobalStyles
             withNormalizeCSS
           >
             <Toaster />
             <Component {...pageProps} />
             <LoginModal />
-
             <Notifications />
           </MantineProvider>
         </ColorSchemeProvider>
