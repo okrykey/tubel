72cf0d3a2469b9c3481f4bfab749ea81ba9f76bc
RyoOkumura <o9mura2000z@gmail.com>
2023-08-07 23:37:30 +0900

    deleted unnecessary components

--- src/server/api/routers/category.ts
+++
@@ -1,8 +0,0 @@
-import { createTRPCRouter, publicProcedure } from "../trpc";
-
-export const categoryRouter = createTRPCRouter({
-  getCategories: publicProcedure.query(async ({ ctx }) => {
-    const categories = await ctx.prisma.category.findMany();
-    return categories.map((category) => category.name);
-  }),
-});
