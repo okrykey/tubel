a48f9975f905bbc195e93fe7c7cd0c48224e1a42
RyoOkumura <o9mura2000z@gmail.com>
2023-08-26 18:10:23 +0900

    feat: Set animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideFromTop: {
+          "0%": { transform: "translateY(1rem)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        slideFromTop: "slideFromTop 0.5s forwards",
+      },
+      transitionDelay: {
+        "100": "100ms",
+        "200": "200ms",
+        "300": "300ms",
+      },
+    },
   },
   plugins: [],
-} satisfies Config;
+};
